803cdd252492846bd7c196bc433c23fdf41f4039
Vibhav <schirrav@usc.edu>
2026-04-23 17:57:54 -0500

    Fixed unicode warnings and networking errors

--- Assets/Scripts/Facilities/BudgetHudResult.cs
+++ Assets/Scripts/Facilities/BudgetHudResult.cs
@@ -1,6 +1,7 @@
 public class BudgetHudResult
 {
     public bool Success;
+    public bool MissingBudgetFile;
     public string Message;
     public decimal Budget;
     public float RecoveryBoostPercent;
